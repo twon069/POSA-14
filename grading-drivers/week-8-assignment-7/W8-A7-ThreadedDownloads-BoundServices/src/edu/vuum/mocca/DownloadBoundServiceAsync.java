27d20d557fd403759b4058ce46440667b040a071
TL.Wong <twon069@users.noreply.github.com>
2014-07-13 15:53:50 +1000

    Revert "W8-A7 fleshed"
    This reverts commit 956b98a12b1490429f0b7f7ad17c6d623348db64.

--- grading-drivers/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadBoundServiceAsync.java
+++ grading-drivers/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadBoundServiceAsync.java
@@ -52,8 +52,6 @@ public class DownloadBoundServiceAsync extends Service{
                 // the appropriate helper method in DownloadUtils and
                 // then send the pathname back to the client via the
                 // callback object.
-            	final String filePath = DownloadUtils.downloadFile(DownloadBoundServiceAsync.this, uri);
-            	callback.sendPath(filePath);
             }
 		
 	};
@@ -77,6 +75,6 @@ public class DownloadBoundServiceAsync extends Service{
     public static Intent makeIntent(Context context) {
         // TODO - replace the null to create the appropriate Intent
         // and return it to the caller.
-    	return new Intent(context, DownloadBoundServiceAsync.class);
+        return null;
     }
 }
